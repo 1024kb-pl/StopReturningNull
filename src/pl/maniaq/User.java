92afe366ed51d1b636a6ed1b461909704d2a3908
klimson <klimson@gitlab.com>
2018-10-24 17:44:31 +0200

    User model

---
+++ src/pl/maniaq/User.java
@@ -0,0 +1,34 @@
+package pl.maniaq;
+
+public class User {
+    private String login;
+    private String email;
+    private String password;
+
+    public User(String login, String email, String password) {
+        this.login = login;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
